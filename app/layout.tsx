c10435bb89e1ba632fe3b6db6a215e97bb8ee72f
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2025-03-27 11:59:31 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Specialized AI for Farmers and Aspiring Farmers",
-  description: "FarmWise is a platform that leverages AI and IoT to transform agriculture in SierraLeone. With our voice assistant agent, Iot Device and a Online Community of Farmers we helped farmers and aspiring farmers engage in sustainable agriculture and precision farming. We help aspiring farmers gets a headstart. We help illiterate farmers with our voice assistant agent to get ai recommendations. With our Iot Devices that monitor crop and soil condition we help farmers make informed decision about their crop. Then with the farmwise community we enable farmers to support each other and connect with a market where they can sell their produce. Overall we answer the questions: What to Plant? When to Plant? and How to Plant?"
+  description: "FarmWise is a platform that leverages AI and IoT to transform agriculture in SierraLeone. With our voice assistant agent, Iot Device and a Online Community of Farmers we helped farmers and aspiring farmers engage in sustainable agriculture and precision farming. We help aspiring farmers gets a headstart. We help illiterate farmers with our voice assistant agent to get ai recommendations. With our Iot Devices that monitor crop and soil condition we help farmers make informed decision about their crop. Then with the farmwise community we enable farmers to support each other and connect with a market where they can sell their produce. Overall we answer the questions: What to Plant? When to Plant? and How to Plant?"}
 
 export default function RootLayout({
   children,
